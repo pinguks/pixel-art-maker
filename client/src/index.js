6f113ff59755bcb81dcad7a21a810bc30dcb1824
Naim <naimm94@gmail.com>
2019-09-14 13:24:01 +0200

    redux devtools added

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
+import { createStore } from "redux";
 import reducers from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers());
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
